ab833f4f793d455c8a449fe9fe467fef49d0c859
dorrion <dhdms8696@naver.com>
2022-12-11 23:58:55 +0900

    [💄DESIGN] 감자 배치

--- client/src/components/Potato/index.tsx
+++ client/src/components/Potato/index.tsx
@@ -4,23 +4,25 @@ import Potato from 'assets/icon/Potato.svg';
 
 const index = () => {
   return (
-    <S.PotatoContainer>
-      <S.IntroBox>
-        <S.PotatoIntro>구르는 감자단 소개</S.PotatoIntro>
-        <S.PotatoSub>
-          프로젝트 사용 설명 및 팀 소개 <Arrow />
-        </S.PotatoSub>
-      </S.IntroBox>
-      <S.PotatoBox>
-        <S.ImgBox className='black' src={Potato} />
-        <S.ImgBox className='medium' src={Potato} />
-        <S.ImgBox className='medium' src={Potato} />
-        <S.ImgBox className='regular' src={Potato} />
-        <S.ImgBox className='regular' src={Potato} />
-        <S.ImgBox className='regular' src={Potato} />
-        <S.ImgBox className='light' src={Potato} />
-      </S.PotatoBox>
-    </S.PotatoContainer>
+    <>
+      <S.PotatoContainer>
+        <S.IntroBox>
+          <S.PotatoIntro>구르는 감자단 소개</S.PotatoIntro>
+          <S.PotatoSub>
+            프로젝트 사용 설명 및 팀 소개 <Arrow />
+          </S.PotatoSub>
+        </S.IntroBox>
+        <S.ImgBox className='s116 righttop visible' src={Potato} />
+        <S.PotatoBox>
+          <S.ImgBox className='s158' src={Potato} />
+          <S.ImgBox className='s116 leftbottom' src={Potato} />
+          <S.ImgBox className='s81 rightbottom' src={Potato} />
+          <S.ImgBox className='s81 rightcorner' src={Potato} />
+          <S.ImgBox className='s81 lefttop' src={Potato} />
+          <S.ImgBox className='s28 center' src={Potato} />
+        </S.PotatoBox>
+      </S.PotatoContainer>
+    </>
   );
 };
 
--- client/src/components/Potato/style.tsx
+++ client/src/components/Potato/style.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { rem } from 'polished';
 
 export const PotatoContainer = styled.div`
@@ -8,9 +8,12 @@ export const PotatoContainer = styled.div`
   background-color: ${({ theme }) => theme.colors.yellow_002};
   border-radius: ${rem(20)};
   margin-top: ${rem(150)};
+  position: relative;
+  overflow: hidden;
 `;
 
 export const IntroBox = styled.div`
+  border: 3px solid red;
   margin-left: ${rem(54)};
   color: ${({ theme }) => theme.colors.black_001};
   filter: none;
@@ -26,24 +29,70 @@ export const PotatoSub = styled.div`
 `;
 
 export const PotatoBox = styled.div`
+  position: absolute; /* 부모 요소 기준으로 absolute */
+  right: 0;
   width: ${rem(495)};
   height: ${rem(150)};
 `;
 
+const DownToUp = keyframes`
+  0% {
+    transform: translateY(${rem(50)});
+  }
+  to {
+    transform: none;
+  }
+`;
+
 export const ImgBox = styled.img`
-  &.black {
+  position: absolute;
+
+  /* &:hover {
+    animation: ${DownToUp} 1s linear;
+  } */
+  &.s158 {
     width: ${rem(158)};
     height: ${rem(158)};
+    left: ${rem(247)};
+    bottom: ${rem(-60)};
   }
-  &.medium {
+  &.s116 {
     width: ${rem(116)};
     height: ${rem(116)};
   }
-  &.regular {
+  &.s81 {
     width: ${rem(81)};
     height: ${rem(81)};
   }
-  &.light {
+
+  &.center {
+    left: ${rem(233)};
+    top: ${rem(52)};
+  }
+  &.lefttop {
+    left: ${rem(245)};
+    top: ${rem(-41)};
+  }
+  &.leftbottom {
+    left: ${rem(114)};
+    bottom: ${rem(-42)};
+  }
+
+  &.rightcorner {
+    right: 0;
+    top: ${rem(3)};
+  }
+
+  &.rightbottom {
+    left: ${rem(402)};
+    bottom: ${rem(-39)};
+  }
+  &.righttop {
+    left: ${rem(326)};
+    top: ${rem(-64)};
+  }
+
+  &.s28 {
     width: ${rem(28)};
     height: ${rem(28)};
   }
