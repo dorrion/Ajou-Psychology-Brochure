fe91cc40b7412ea2209c2f15b2b99e8b7a11d77a
dorrion <dhdms8696@naver.com>
2022-12-13 21:41:18 +0900

    [✨FEAT] Storybook에 Theme와 전역스타일 추가

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,9 +1,23 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from 'styles/GlobalStyle';
+import { theme } from 'styles/Theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+// Styled-Component를 사용하기 위한 설정
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Story />
+    </ThemeProvider>
+  ),
+];
