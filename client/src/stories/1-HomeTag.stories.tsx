5d12e3f1c0fe84ece81777135b09370948c83130
dorrion <dhdms8696@naver.com>
2022-12-14 18:17:18 +0900

    [➖REMOVE] 필요없는 파일 삭제

--- client/src/stories/1-HomeTag.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Tag from 'components/Tag';
-
-export default {
-  title: 'HomePage', // 스토리북에서 보여질 그룹과 경로를 명시
-  component: Tag, // 어떤 컴포넌트를 문서화 할지 명시
-};
-
-export const tag = () => <Tag content='이수학점' />;
