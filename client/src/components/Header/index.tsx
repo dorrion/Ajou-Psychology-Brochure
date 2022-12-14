8890bd99340edc838f9dd5e022ac2e3d6ea05043
dorrion <dhdms8696@naver.com>
2022-12-15 03:37:19 +0900

    [✨FEAT] 헤더 라우팅

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,14 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 
-const index = () => {
+const Index = () => {
+  let navigate = useNavigate();
+  const navigateIntroduce = () => {
+    navigate('/Introduce');
+  };
+  const navigateGraduate = () => {
+    navigate('/Graduate');
+  };
+  const navigateEmployment = () => {
+    navigate('/Employment');
+  };
+  const navigateAbroad = () => {
+    navigate('/Abroad');
+  };
+
   return (
     <S.HeaderContainer>
-      <S.HeaderMenu>소개</S.HeaderMenu>
-      <S.HeaderMenu>대학원</S.HeaderMenu>
-      <S.HeaderMenu>취업</S.HeaderMenu>
-      <S.HeaderMenu>유학</S.HeaderMenu>
+      <S.HeaderMenu onClick={navigateIntroduce}>소개</S.HeaderMenu>
+      <S.HeaderMenu onClick={navigateGraduate}>대학원</S.HeaderMenu>
+      <S.HeaderMenu onClick={navigateEmployment}>취업</S.HeaderMenu>
+      <S.HeaderMenu onClick={navigateAbroad}>유학</S.HeaderMenu>
     </S.HeaderContainer>
   );
 };
 
-export default index;
+export default Index;
