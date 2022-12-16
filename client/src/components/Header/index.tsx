3c974d591698b6b6e75b654f56b63b9992591303
dorrion <dhdms8696@naver.com>
2022-12-16 20:00:31 +0900

    [✨FEAT] 라우팅 추가

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -2,26 +2,14 @@ import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 
 const Index = () => {
-  let navigate = useNavigate();
-  const navigateIntroduce = () => {
-    navigate('/Introduce');
-  };
-  const navigateGraduate = () => {
-    navigate('/Graduate');
-  };
-  const navigateEmployment = () => {
-    navigate('/Employment');
-  };
-  const navigateAbroad = () => {
-    navigate('/Abroad');
-  };
+  const navigate = useNavigate();
 
   return (
     <S.HeaderContainer>
-      <S.HeaderMenu onClick={navigateIntroduce}>소개</S.HeaderMenu>
-      <S.HeaderMenu onClick={navigateGraduate}>대학원</S.HeaderMenu>
-      <S.HeaderMenu onClick={navigateEmployment}>취업</S.HeaderMenu>
-      <S.HeaderMenu onClick={navigateAbroad}>유학</S.HeaderMenu>
+      <S.HeaderMenu onClick={() => navigate('/Introduce')}>소개</S.HeaderMenu>
+      <S.HeaderMenu onClick={() => navigate('/Graduate')}>대학원</S.HeaderMenu>
+      <S.HeaderMenu onClick={() => navigate('/Employment')}>취업</S.HeaderMenu>
+      <S.HeaderMenu onClick={() => navigate('/Abroad')}>유학</S.HeaderMenu>
     </S.HeaderContainer>
   );
 };
