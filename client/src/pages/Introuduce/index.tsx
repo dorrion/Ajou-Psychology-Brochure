a0a5d26f4b0d87cd2dd12bcd002724f23532e72a
dorrion <dhdms8696@naver.com>
2022-12-21 03:22:11 +0900

    [✨FEAT] 소개 페이지 마크업

--- client/src/pages/Introuduce/index.tsx
+++ client/src/pages/Introuduce/index.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-
-function index() {
+import IntroBox from 'components/IntroBox';
+import { rem } from 'polished';
+import styled from 'styled-components';
+export default function index() {
   return (
-    <div>
-      
-    </div>
+    <IntroContainer>
+      <IntroBox>
+        <Info>
+          <h1>구르는 감자단 소개</h1>
+          <div>프로젝트 사용 설명 및 팀 소개</div>
+        </Info>
+      </IntroBox>
+    </IntroContainer>
   );
 }
 
-export default index;
+const IntroContainer = styled.div`
+  margin-top: ${rem(40)};
+  ${({ theme }) => theme.mixins.flexBox('column', 'center', 'center')}
+`;
+
+const Info = styled.div`
+  ${({ theme }) => theme.mixins.flexBox('column', 'center', 'center')}
+`;
