6b169354804860c50c915ad0b7b49df3dd6499ac
dorrion <dhdms8696@naver.com>
2022-12-21 03:58:15 +0900

    [🎨STYLE] 정렬 수정

--- client/src/components/IntroBox/style.tsx
+++ client/src/components/IntroBox/style.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { rem } from 'polished';
 
 export const PotatoContainer = styled.div`
-  ${({ theme }) => theme.mixins.flexBox('row', 'center', 'center')}
   width: ${rem(1280)};
   height: ${rem(248)};
   background-color: ${({ theme }) => theme.colors.colorSecondary};
